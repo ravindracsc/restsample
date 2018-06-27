83da1af3e68c870b91327225b0c0056d83ab0214
ravindracsc <36528498+ravindracsc@users.noreply.github.com>
2018-06-27 18:26:02 +0530

    power shell script & vpkg files added

---
+++ restsample/PowerShellScript.ps1
@@ -0,0 +1,13 @@
+###############################################################
+# This script will do the following:                          #
+# - Deploy the vpkg files specified to viaduct environment 	  #
+#                                                             #
+# To use the script :                                         #
+# 							                                  #
+#                                                             #
+# Created by nath ravindra 27/08/2018                         #
+###############################################################
+
+
+cd  D:\Next-Generation\Next-Generation\API\LorenzoCareSuite\DevOps\OHC
+java -jar vclu.jar --deploy --jmx --username admin --password Abc123$$$ --address 127.0.0.1 --port 1617 --filename RequestAdapter.vpkg --flush
